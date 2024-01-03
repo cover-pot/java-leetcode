48c27461a0dd81e33fa7daabbabdd22ec08460f0
potcover <bo.dou626@gmail.com>
2024-01-03 23:45:49 +0800

    top-interview-150.26

---
+++ src/top_interview_150/RemoveDuplicates.java
@@ -0,0 +1,26 @@
+package top_interview_150;
+
+import java.util.Queue;
+
+/**
+ * Created by cover at 2024/1/3
+ */
+public class RemoveDuplicates {
+    public int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int l = 0;
+        int idx = 1;
+
+        while (idx < nums.length) {
+            if (nums[l] != nums[idx]) {
+                nums[l + 1] = nums[idx];
+                l++;
+            }
+            idx++;
+        }
+
+        return l + 1;
+    }
+}
